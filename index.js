dd00cc31f2b9dbadcd9b6543ed0378755dc4c439
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-02 00:04:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks (blockNumber) {
+  if (blockNumber > 42) {
+    return blockNumber - 42;
+  } else {
+    return 42 - blockNumber;
+  }
+}
