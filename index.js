8afb74c4d484fed29432b2b7f20d3ace4f080b04
Ryan Schmelter <rschmelter33@gmail.com>
2018-08-02 00:12:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function calculatesFarePrice (start, destination) {
   if (distance <= 400) {
     return 0;
   } else if (distance > 400 && distance <= 2000) {
-    return .02 * distance;
+    let fare = .02 * distance = 8;
+    return fare;
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else {
